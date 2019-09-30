22360bfe6a90e92190f8a67bea0cb84b15a23ef6
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-30 10:13:54 +0900

    [課題G(途中)].csvファイルを操作するための準備
    - Pathモジュールをインポート
    - school_data内の.csvファイルのデータを操作するために、~~.csvのファイルのパスを全て検索してPathオブジェクトのリストを作成。今後これを利用してファイルの書き換えを行う。

--- include_header.py
+++ include_header.py
@@ -1,5 +1,16 @@
+from pathlib import Path
+
 def main():
-    pass
+    # 各ディレクトリ内のCSVファイルの取得
+
+    # 全てのcsvファイルを使用するために、
+    # school_data内の*.csvファイルのPathディレクトリオブジェクトのリストを作成
+    csv_path = Path('.')
+    csv_path = list(csv_path.glob('**/**/*.csv'))
+
+    # ヘッダーの追加
+    # 各ディレクトリ内のCSVファイルに書き込み
+
 
 
 if __name__ == '__main__':
