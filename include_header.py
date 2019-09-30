173b8e159bf930da7e420a8a7033b783aa9700ce
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-30 11:47:12 +0900

    [課題G]ヘッダの追加したファイルの作成（ひとまず完成）
    - 元ファイルを保持するために出力用にコピーを作成
    - コピーのディレクトリも取得して、ヘッダーを加えたうえでファイルをコピー
    
    (問題点):
    ①一度全てのデータをコピー→ヘッダーを追記して再度データをコピー
     と2度手間になっている。
    ②ディレクトリのリストが二つあるので対応関係が保証されていない
    
    →オリジナルのディレクトリからコピーを直接作れるといいなぁ

--- include_header.py
+++ include_header.py
@@ -1,15 +1,31 @@
+import csv
+import shutil
 from pathlib import Path
 
+
 def main():
-    # 各ディレクトリ内のCSVファイルの取得
+    # cpy_score_data内の全てのcsvファイルを操作するために、
+    # ~~.csvというファイルのPathディレクトリオブジェクトのリストを作成
+    csv_origin_paths = Path('score_data')
+    csv_origin_paths = list(csv_origin_paths.glob('**/*.csv'))
 
-    # 全てのcsvファイルを使用するために、
-    # school_data内の*.csvファイルのPathディレクトリオブジェクトのリストを作成
-    csv_path = Path('.')
-    csv_path = list(csv_path.glob('**/**/*.csv'))
+    # 元ファイルを保持するためにコピーを作成
+    shutil.copytree('score_data', 'cpy_score_data')
+    csv_cpy_paths = Path('cpy_score_data')
+    csv_cpy_paths = list(csv_cpy_paths.glob('**/*.csv'))
 
-    # ヘッダーの追加
-    # 各ディレクトリ内のCSVファイルに書き込み
+    # コピーしたファイルにヘッダーを追加する
+    for data_num in range(len(csv_origin_paths)):
+        with open(csv_origin_paths[data_num], 'r', encoding='utf-8') as f:
+            with open(csv_cpy_paths[data_num], 'w', encoding='utf-8') as cf:
+                # ヘッダーの追加
+                writer = csv.writer(cf, lineterminator='\n')
+                writer.writerow(['氏名', 'メールアドレス', '得点'])
+
+                # 元ファイルのからデータの読み込みとコピー先に書き込み
+                reader = csv.reader(f)
+                for line in reader:
+                    writer.writerow(line)
 
 
 
